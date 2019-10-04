6f16d02d45063afc8d662aa6baab02ecb2a58247
MirajBappy <MirajBappy>
2019-10-04 14:29:55 +0600

    2nd commit: default auth, migration, seeder

---
+++ project/database/seeds/AdminsTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class AdminsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('admins')->insert([
+            'name' => 'System Admin',
+            'email' => 'systemadmin@gmail.com',
+            'password' => bcrypt('password'),
+            'user_type' => 'system_admin',
+            'created_at' => date('Y-m-d H:i:s'),
+        ]);
+    }
+}
